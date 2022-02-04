5815b6653822000ab1151a4c5a7d5c1f7c09b32a
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2022-02-04 14:28:30 +0700

    j

--- src/main/java/com/algorithmlx/liaveres/common/event/CraftingEvent.java
+++ src/main/java/com/algorithmlx/liaveres/common/event/CraftingEvent.java
@@ -2,34 +2,45 @@ package com.algorithmlx.liaveres.common.event;
 
 import com.algorithmlx.liaveres.common.LiaVeres;
 import com.algorithmlx.liaveres.common.setup.registries.Registration;
-import net.minecraft.core.Vec3i;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
-import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
 
-@Mod.EventBusSubscriber(modid = LiaVeres.ModId, bus = Mod.EventBusSubscriber.Bus.MOD)
+import java.util.List;
+
+//FIXME
+
+//@Mod.EventBusSubscriber(modid = LiaVeres.ModId, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CraftingEvent {
-    @SubscribeEvent(priority = EventPriority.NORMAL)
-    public void init(EntityStruckByLightningEvent event) {
-        lightningBolt(event);
-    }
+//    @SubscribeEvent
+    public void init(EntityJoinWorldEvent event) {
+        Entity e = event.getEntity();
+        EntityType<?> et = e.getType();
+        if (et == EntityType.LIGHTNING_BOLT) {
+            BlockPos pos = event.getEntity().blockPosition();
+            Level level = event.getWorld();
 
-    private void lightningBolt(@NotNull EntityStruckByLightningEvent event) {
-        Level level = event.getLightning().getLevel();
-        if (event.getEntity() instanceof ItemEntity entity) {
-            Item item = entity.getItem().getItem();
-            int x = entity.getBlockX();
-            int y = entity.getBlockY();
-            int z = entity.getBlockZ();
-            if (item == Registration.LIGHTNING_ARTIFACT.get()) {
-                level.getEntities(entity, AABB.ofSize(Vec3.atCenterOf(Vec3i.ZERO), x, y, z));
+            ItemEntity itemEntity = (ItemEntity) e;
+            Item item = itemEntity.getItem().getItem();
+
+            if (!(item == Registration.EMPTY_ARTIFACT.get())) {
+                int x = itemEntity.getBlockX();
+                int y = itemEntity.getBlockY();
+                int z = itemEntity.getBlockZ();
+
+                List<ItemEntity> itemEntityList =
+                        level.getEntitiesOfClass(ItemEntity.class, new AABB(pos.mutable().move(x, y, z)));
+                for (ItemEntity entity : itemEntityList) {
+                    if (entity.getItem().getItem() == Registration.EMPTY_ARTIFACT.get())
+                        entity.spawnAtLocation(Registration.LIGHTNING_ARTIFACT.get(), 1);
+                }
             }
         }
     }
--- src/main/java/com/algorithmlx/liaveres/common/item/artifact/LightningArtifact.java
+++ src/main/java/com/algorithmlx/liaveres/common/item/artifact/LightningArtifact.java
@@ -14,29 +14,32 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
 
 public class LightningArtifact extends Item {
     public LightningArtifact() {
-        super(new Properties().durability(10000).tab(ModSetup.ARTIFACT_TAB).rarity(Rarity.EPIC));
+        super(new Properties().durability(10000).tab(ModSetup.ARTIFACT_TAB).rarity(Rarity.EPIC).fireResistant());
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(Level world, Player player, @NotNull InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
         Vec3 start  = player.getEyePosition(1);
         Vec3 vec31 = player.getViewVector(1);
         Vec3 end = start.add(vec31.x * 5, vec31.y * 5, vec31.z * 5);
         ClipContext pos = new ClipContext(start, end, ClipContext.Block.OUTLINE, ClipContext.Fluid.ANY, player);
 
-        double i = pos.getTo().x;
-        double j = pos.getTo().y;
-        double k = pos.getTo().z;
+        double x = pos.getTo().x;
+        double y = pos.getTo().y;
+        double z = pos.getTo().z;
 
-        world.getBlockState(new BlockPos(i, j, k));
-        LightningBolt bolt1 = new LightningBolt(EntityType.LIGHTNING_BOLT, world), bolt2 = new LightningBolt(EntityType.LIGHTNING_BOLT, world), bolt3 = new LightningBolt(EntityType.LIGHTNING_BOLT, world);
-        bolt1.moveTo(i,j,k);
-        bolt2.moveTo(i,j,k);
-        bolt3.moveTo(i,j,k);
+        world.getBlockState(new BlockPos(x, y, z));
+        LightningBolt bolt1 = new LightningBolt(EntityType.LIGHTNING_BOLT, world),
+                bolt2 = new LightningBolt(EntityType.LIGHTNING_BOLT, world),
+                bolt3 = new LightningBolt(EntityType.LIGHTNING_BOLT, world);
+        bolt1.moveTo(x,y,z);
+        bolt2.moveTo(x,y,z);
+        bolt3.moveTo(x,y,z);
         world.addFreshEntity(bolt1);
         world.addFreshEntity(bolt2);
         world.addFreshEntity(bolt3);
@@ -45,8 +48,6 @@ public class LightningArtifact extends Item {
                 p_220009_1_.broadcastBreakEvent(player.getUsedItemHand());
             });
         }
-
-        return new InteractionResultHolder<ItemStack>(InteractionResult.SUCCESS, stack);
-
+        return InteractionResultHolder.success(stack);
     }
 }
